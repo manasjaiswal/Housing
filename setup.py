da4bc7468f053ee06599dc5b4e79e6197d5ed130
manasjaiswal <mjswl12@gmail.com>
2022-10-28 12:24:18 +0530

    folder_structure

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 def get_requirements_list()->List[str]:
     with open('requirements.txt') as req_file:
-        return req_file.readlines()
+        return req_file.readlines().remove("-e .)
 
 PROJECT_NAME="housing-predictor"
 AUTHOR="ManasJaiswal"
 Description="This is a first project based on CI/CD pipelines"
-Packages=["Housing"]
 
 setup(
 name=PROJECT_NAME,
 author=AUTHOR,
 description=Description,
-packages=Packages,
+packages=find_packages(),
 install_requires=get_requirements_list()
 
 )
